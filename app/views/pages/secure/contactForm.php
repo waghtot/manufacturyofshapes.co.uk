9007429b41b0983bd02062719159c507bc62f707
MariuszWM <mariusz.marciniak@tuxedomoney.com>
2019-03-11 23:10:13 +0000

    tekst polski

--- app/views/pages/secure/contactForm.php
+++ app/views/pages/secure/contactForm.php
@@ -1,11 +1,15 @@
 <form id="enquiry" name="enquiry">
+<div class="form-group">
+        <input type="email" class="form-control" id="u_fullName" aria-describedby="nameHelp" placeholder="Full Name" name="name">
+    </div>
     <div class="form-group">
-        <label for="Email">Email</label>
         <input type="email" class="form-control" id="u_email" aria-describedby="emailHelp" placeholder="Email address" name="email">
     </div>
     <div class="form-group">
-        <label for="query">Enquiry</label>
-        <textarea class="form-control enquiry-form" id="u_query" aria-describedby="emailHelp" placeholder="Your enquiry" name="query"></textarea>
+        <input type="text" class="form-control" id="u_phone" aria-describedby="phoneHelp" placeholder="Phone Number" name="phone">
     </div>
-    <button id="send" class="btn btn-md btn-secondary btn-right">Send</button>
+    <div class="form-group">
+        <textarea class="form-control enquiry-form" id="u_query" placeholder="Your enquiry" name="query"></textarea>
+    </div>
+    <button id="send" class="btn btn-md btn-secondary btn-main btn-right">Send</button>
 </form>
--- app/views/pages/secure/home.php
+++ app/views/pages/secure/home.php
@@ -1,30 +1,59 @@
 <div class="container space" id="About_Us">
-    <h1>About Us</h1>
-        <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-        </p>    
+    <p class="quotation">
+    Łańcuchy rutyny są zbyt lekkie, aby je odczuwać,<br>dopóki nie staną się zbyt ciężkie, aby je zerwać.
+    <p class="center">Warren Buffett</p>
+    <div class="quotation-line mx-auto"></div>
+    </p>
+
+
+    <!-- <p class="quotation">
+    Sukces jest zdolnością do przejścia od jednej porażki do drugiej<br>bez utraty entuzjazmu.
+    <p class="center">Winston Churchill</p>
+    <div class="quotation-line mx-auto"></div>
+    </p> -->
+    <h1>O Nas</h1>
+    <p>
+        Manufaktura Kształtów nie jest nazwą przypadkową. Trochę czasu zajęło nam znalezienie tej właściwej, w której moglibyśmy zamknąć nasze dotychczasowe dokonania i wykonane projekty. Różnoworność prac jakie wykonaliśy 
+
+        Pomagamy i tworzymy rozwiazania szyte na miarę potrzeb naszych klientów. W opraciu o zdobytą wiedzę i doswiadczenie w pracy z mediami drukowanymi oraz cyfrowymi postanowiliśmy rozpocząć własną przygodę oferując swoje usługi w tym zakresie wszystkim tym, którzy myślą o nadaniu swojemu biznesowu rozpoznawalnego kształtu i koloru. Manufaktura kształtów nie jest tutaj nazwą przypadkową. Wzięła się z różnorodności prac i projektów jakie wykonaliśmy w przeciągu ostatnich lat. Od wizualizacji danych przez projektowanie, przygotowanie do druku szerokiej gamy materiałów.  
+    </p>    
 </div>
 
 <div class="container-fluid mos" id="Websites">
     <div class="container space">
 
         <div class="row">
-
             <div class="col-6">
-                <h1 class="white">£100 Websites</h1>
+                <h1 class="white">£100 za stronę?</h1>
                 <p class="white">
-                    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
+                    Strona za "stówkę", to nasz propozycja dla małych jak i dopiero startujących firm, które chcą stać się bardziej widoczne dla swoich obecnych oraz przyszłych klientów. To produkt nie wymagający od firmy dużego zaangażowania czasu i środków, w stworzenie czytelnego przekazu. Powiedz nam kim jesteś i czym się zajmujesz a my pomożemy ci przygotować łatwo dostępną i czystą informację.
                 </p>
             </div>
 
             <div class="col-6">
-                <h1 class="white">Random Forms</h1>
+                <h1 class="white">W kontakcie...</h1>
                 <p class="white">
-                    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
+                    Kontakt z klientem jest rdzeniem każdej działalności bez względu na jej wielkość. "W kontakcie..." jest pomocą w realizacji tej części działalności twojej firmy. Jeśli masz już swoją stronę i myślisz o wzbogaceniu jej o kalendarz, dzięki któremu twoi klienci będą mogli zabukować spotkanie lub najdogodniejszy dla nich termin wykonania zlecenia, formularz kontaktowy lub oceny czy opinii? Tutaj też mamy wiele do zaoferowania.
                 </p>
             </div>
-
         </div>
+
+                <div class="row">
+            <div class="col-6">
+                <h1 class="white">Rozwiązanie na miarę</h1>
+                <p class="white">
+                    W zależności od tego czego potrzebujesz, służymy radą i pomysłami na przygotowanie możliwie najlepszego rozwiązania. Jeśli twoja aplikacja wymaga gromadzenia i przetwarzania informacji z jednego lub więcej źródeł. Planujesz wymianę danych z zewnętrznymi żródłami lub klientami w różnych formatach. Tutaj też możemy służyć swoją wiedzą i doświadczeniem. Pomożemy ci na dowolnym etapie pracy od projektu po wykonanie. 
+                </p>
+            </div>
+
+            <div class="col-6">
+                <h1 class="white">Ważne i ważniejsze</h1>
+                <p class="white">
+                    Jeśli myślisz o wyróżnienu jakiegoś produktu czy usługi bez ingerencji w już istniącą stronę. Jako osobny element z powodu jego, sezonowości lub niepowtarzalności. Chetnie pomożemy w przygotowaniu materiałów do uruchomienia takiej informacji w dowolnej postaci. Jako mikrostrony w obrębie twojej głównej domeny, materiału do emisji w mediach społecznościowych, prezentacji lub w formie baneru reklamowego.  
+                </p>
+            </div>
+        </div>
+
     </div>
 </div>
 
@@ -40,7 +69,13 @@
         <div class="col-6">
             <h1>&nbsp;</h1>
             <p>
-                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+                Zapraszamy do kontaktu.
+            <p>
+            <p>
+                Jeśli masz jakieś pytania lub chcesz dowiedzieć się o nas nieco więcej. Prześlij nam swoje pytania lub informacje dotyczące projektu lub strony w tworzeniu których moglibyśmy służyć swoim doświadczeniem.
+            <p>
+            </p>
+                Na każde pytanie postramy się odpowiedzieć możliwie najszybciej.
             </p>
         </div>
     </div>
