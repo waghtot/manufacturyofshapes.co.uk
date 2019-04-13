74d94ae412f33105c4e8ce908df7fc7547a317f3
waghtot <waghtot@gmail.com>
2019-04-13 23:28:05 +0100

    email check

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -19,19 +19,24 @@ class Home {
     }
 
     public function enquiry(){
+
         if(isset($_POST)){
 
-
-            $to = "waghtot@gmail.com";
+            $to = "info@manufacturyofshapes.co.uk";
             $subject = "Enquiry";
+            $messge = 'This is test message';
             $txt = $_POST['message'];
             $headers = "MIME-Version: 1.0" . "\r\n";
             $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            $headers .= "From: <".$_POST['email'].">". "\r\n" .
-            "CC: waghtot@gmail.com";
-            
-            mail($to,$subject,$txt,$headers);
+            // $headers .= "From: <".$_POST['email'].">". "\r\n" . "CC: waghtot@gmail.com";
 
+            // error_log('email header: '.print_r($headers, 1));
+            
+            // mail($to,$subject,$txt,$headers);
+            // error_log('Send email: '.print_r(mail($to,$subject,$txt,$headers), 1));
+            // imap_mail($to, $subject, $messge, $headers);
+            mail($to, $subject, $messge, $headers);
+            error_log('Send email: '.print_r(mail($to, $subject, $messge, $headers), 1));
             echo json_encode('ok');
         }
 
--- index.php
+++ index.php
@@ -2,6 +2,25 @@
 session_start();
 require_once "vendor/autoload.php";
 require_once "app/config/constants.php";
-new Router();
+
+
+$to      = 'info@manufacturyofshapes.co.uk';
+$subject = 'Test meassage';
+$message = 'hello';
+$headers = 'From: waghtot@gmail.com' . "\r\n" .
+	    'Reply-To: waghtot@gmail.com' . "\r\n" .
+	        'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+
+// $success = mail($to, $subject, $message, $headers);
+// if (!$success) {
+//     $errorMessage = error_get_last()['message'];
+// }
+
+// error_log('email sent: '.print_r($errorMessage, 1));
+
+//new Router();
 
 ?>
